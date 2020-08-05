687f8fbf1b89f74f4dc71a0080a1bdb2c88a6506
Ankit Pramanik <pramanik.85849@gmail.com>
2020-08-05 08:43:38 +0530

    server createdusing socket

---
+++ nodeserver/index.js
@@ -0,0 +1,13 @@
+const io = require('socket.io')(3600)
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        user[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    })
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', { message: message, name: user[socket.id] })
+    });
+})
